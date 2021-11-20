c8021049c4747f03c0032f08e136676e6080906a
wolfy1339 <webmaster@wolfy1339.com>
2021-11-20 16:40:46 -0500

    Add typings for `voiceStateUpdate()`

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,7 +1,8 @@
 import utils from '../utils';
 import { queueManager } from '../queue.js';
+import { VoiceState } from 'discord.js';
 
-export default async (oldVoiceState, newVoiceState) => {
+export default async (oldVoiceState: VoiceState, newVoiceState: VoiceState) => {
     let serverQueue = queueManager.get(newVoiceState.guild.id);
     if (serverQueue === undefined) return;
 
