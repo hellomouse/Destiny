833cfdd6d1919bd9a16cad0fedd5ab956ad82a92
wolfy1339 <webmaster@wolfy1339.com>
2021-11-19 20:54:57 -0500

    Fix `config.d.ts`: local module vs external module
    Fixes the following error message from TypeScript:
    
    ```
    File 'Programming/Destiny/config.d.ts' is not a module.ts(2306)
    ```
    
    This happens when you are defining a local module as an external module. If you are trying to define a local module, you do not need a `define module "foo" {}` block

--- config.d.ts
+++ config.d.ts
@@ -1,20 +1,18 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-declare module 'config' {
-    export default class config {
-        token: string;
-        prefix: string;
-        inactivity: {
-            waitRejoinSeconds: number;
-            botIdleSeconds: number;
-        };
-        songManager: {
-            softNumLimit: number;
-            hardNumLimit: number;
-            cacheCleanTimeoutDuration: number;
-            metadataRefreshInterval: {
-                YouTubeSong?: number;
-            }
-        };
-        allowed: Array<string>;
-    }
+export default class config {
+    token: string;
+    prefix: string;
+    inactivity: {
+        waitRejoinSeconds: number;
+        botIdleSeconds: number;
+    };
+    songManager: {
+        softNumLimit: number;
+        hardNumLimit: number;
+        cacheCleanTimeoutDuration: number;
+        metadataRefreshInterval: {
+            YouTubeSong?: number;
+        }
+    };
+    allowed: Array<string>;
 }
