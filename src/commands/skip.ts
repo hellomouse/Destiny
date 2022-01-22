f76fe6d4e75c21282060ca2e543ce478a5abda5b
wolfy1339 <webmaster@wolfy1339.com>
2022-01-21 21:05:34 -0500

    Add an ammout option to the skip command
    Allow to skip X number of songs
    
    Co-Authored-By: Bradley Bellairs <11426553+BWBellairs@users.noreply.github.com>

--- src/commands/skip.ts
+++ src/commands/skip.ts
@@ -1,6 +1,6 @@
-import { log } from '../utils';
-import { songEmbed, queueNotPlaying } from '../embeds.js';
-import { queueManager } from '../queue.js';
+import { log } from '../utils.js';
+import { songEmbed, errorEmbed, queueNotPlaying } from '../embeds.js';
+import { LOOP_MODES, queueManager } from '../queue.js';
 import COMMAMD_REQUIREMENTS from '../commands.js';
 import { Client, Message } from 'discord.js';
 
@@ -13,8 +13,26 @@ import { Client, Message } from 'discord.js';
  */
 export const run = async (client: Client, message: Message, args: Array<string>) => {
     const serverQueue = queueManager.get(message.guild!.id)!;
-    log(`Skipped music : ${serverQueue!.songs[0].song.title}`);
-    serverQueue!.skip();
+
+    log(`Skipped music : ${serverQueue.songs[0].song.title}`); // Problem is here
+
+
+    let skipAmount: number = +args[0] ? +args[0] + 1 : 0;
+    let skipTo = 0;
+
+    // If we're looping the queue, find the index of the song to jump to
+    if (serverQueue.loop === LOOP_MODES['QUEUE'])
+        skipTo = serverQueue.getIndex() + skipAmount % serverQueue.size();
+    else if (skipAmount > (serverQueue.size() - serverQueue.getIndex()))
+        // Can't skip songs if it takes us out of the queue when not looping it
+        return await message.reply({ embeds: [errorEmbed().setDescription('Value out of bounds')] });
+    else
+        skipTo = serverQueue.getIndex() + skipAmount;
+
+    if (skipTo)
+        serverQueue.jump(skipTo);
+    else
+        serverQueue.skip();
 
     const currentSong = serverQueue.currentSong();
     if (currentSong)
@@ -26,6 +44,6 @@ export const run = async (client: Client, message: Message, args: Array<string>)
 export const names = ['skip', 's'];
 export const help = {
     desc: 'Skip the current song',
-    syntax: ''
+    syntax: '[amount]'
 };
 export const requirements = COMMAMD_REQUIREMENTS.REQUIRE_QUEUE_NON_EMPTY | COMMAMD_REQUIREMENTS.REQUIRE_USER_IN_VC;
