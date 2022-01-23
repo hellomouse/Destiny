97b12e586f764fe64313824cd49c23b38dcb7985
Bradley Bellairs <BWBellairs@outlook.com>
2022-01-23 01:13:08 +0000

    Add clear command
    Signed-off-by: Bradley Bellairs <BWBellairs@outlook.com>

---
+++ src/commands/clear.ts
@@ -0,0 +1,26 @@
+import { defaultEmbed } from '../embeds.js';
+import { queueManager } from '../queue.js';
+import COMMAMD_REQUIREMENTS from '../commands.js';
+import { Client, Message } from 'discord.js';
+
+/**
+ * @description Clears the queue
+ * @param {Discord.Client} client the client thats runs the commands
+ * @param {Discord.Message} message the command's message
+ * @param {Array<string>} args Unused
+ * @return {Promise<Message>} sent message
+ */
+ export const run = async (client: Client, message: Message, args: Array<string>) => {
+    const serverQueue = queueManager.get(message.guild!.id)!;
+
+    serverQueue.clear(false);
+
+    return message.reply({ embeds: [defaultEmbed().setTitle('Cleared').setDescription('The queue has been cleared')] });
+};
+
+export const names = ['clear'];
+export const help = {
+    desc: 'Clears the queue',
+    syntax: ''
+};
+export const requirements = COMMAMD_REQUIREMENTS.REQUIRE_QUEUE_NON_EMPTY | COMMAMD_REQUIREMENTS.REQUIRE_USER_IN_VC;
