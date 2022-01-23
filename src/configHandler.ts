1379464d968957a8b22c4b5704dcd112f4dad73b
wolfy1339 <webmaster@wolfy1339.com>
2022-01-22 20:51:22 -0500

    Add type information to `configHandler()`
    Instead of returning `Promise<any>` it now returns the type of the config with defaults

--- src/configHandler.ts
+++ src/configHandler.ts
@@ -1,29 +1,32 @@
+import type { Config } from '../config';
+
 export async function configHandler() {
-    let config = (await import(`../config.cjs?ts=${Date.now()}`)).default;
-    config = Object.assign(
-        {
-            token: process.env.TOKEN,
-            prefix: process.env.PREFIX || '!!',
-            inactivity: {
-                waitRejoinSeconds: 60,
-                botIdleSeconds: 600
-            },
-            songManager: {
-                softNumLimit: 1000000,
-                hardNumLimit: 10000000,
-                cacheCleanTimeoutDuration: 300,
-                metadataRefreshInterval: {
-                    // eslint-disable-next-line @typescript-eslint/naming-convention
-                    YouTubeSong: 3600
-                }
-            },
-            allowed: process.env.ALLOWED ? [process.env.ALLOWED] : [] // TODO: rework permissions
+    let config: Config = (await import(`../config.cjs?ts=${Date.now()}`)).default;
+    const defaults = {
+        token: process.env.TOKEN,
+        prefix: process.env.PREFIX || '!!',
+        inactivity: {
+            waitRejoinSeconds: 60,
+            botIdleSeconds: 600
         },
+        songManager: {
+            softNumLimit: 1000000,
+            hardNumLimit: 10000000,
+            cacheCleanTimeoutDuration: 300,
+            metadataRefreshInterval: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                YouTubeSong: 3600
+            }
+        },
+        allowed: process.env.ALLOWED ? [process.env.ALLOWED] : [] // TODO: rework permissions
+    };
+    config = Object.assign(
+        defaults,
         config
     );
 
     if (!config.token)
         throw new Error('Bot token not defined in config or environment');
 
-    return config;
+    return config as typeof defaults & { token: string };
 }
