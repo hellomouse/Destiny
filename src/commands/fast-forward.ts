0974eb9da355d1b6ecdbea7f900b6c9ce6934783
wolfy1339 <webmaster@wolfy1339.com>
2022-02-02 19:47:42 -0500

    Add fast forward command
    Reuses the code from the seek command with one slight modification

---
+++ src/commands/fast-forward.ts
@@ -0,0 +1,57 @@
+import { defaultEmbed, errorEmbed } from '../embeds.js';
+import { FlagHelpError, formatDuration, log } from '../utils.js';
+import { queueManager } from '../queue.js';
+import COMMAMD_REQUIREMENTS, { CommandArgument, CommandArgumentNecessity, CommandHelpProvider } from '../commands.js';
+import type { Client } from '../types';
+import type { Message } from 'discord.js';
+
+/**
+ * @description Fast-forward a given amount of time
+ * @param {Discord.Client} client the client thats runs the commands
+ * @param {Discord.Message} message the command's message
+ * @param {Array<string>} args Unused
+ * @return {Promise<Message>} sent message
+ */
+export const run = async (client: Client, message: Message, args: Array<string>) => {
+    if (!args[0])
+        throw new FlagHelpError();
+
+    const serverQueue = queueManager.get(message.guild!.id)!;
+
+    if (serverQueue.isIdle()) return message.channel.send({ embeds: [errorEmbed().setDescription('Nothing is playing right now')] });
+
+    let seekTime = +args[0];
+    if (Number.isNaN(seekTime)) {
+        // Try to match format: XXhXXm or AA:BB:CC
+        const getN = (match: RegExpMatchArray, n: number) => match && match[n] ? +match[n].replace(/[^0-9]/g, '') : 0;
+        const TIMESTAMP_REGEX_1 = /^(\d+:)?(\d+):(\d+)$/im;
+        const TIMESTAMP_REGEX_2 = /^(\d+h)?(\d+m)?(\d+s)?$/im;
+
+        let m = args[0].match(TIMESTAMP_REGEX_1);
+        if (m) seekTime = getN(m, 1) * 60 * 60 + getN(m, 2) * 60 + getN(m, 3);
+
+        if (!m) {
+            m = args[0].match(TIMESTAMP_REGEX_2);
+            if (m) seekTime = getN(m, 1) * 60 * 60 + getN(m, 2) * 60 + getN(m, 3);
+        }
+        if (!m)
+            return message.reply({
+                embeds: [errorEmbed()
+                    .setTitle(`Invalid seek parameter \`${args[0]}\``)]
+            });
+    }
+
+    seekTime += serverQueue.audioResource!.playbackDuration / 1000;
+    seekTime = Math.max(0, seekTime);
+    seekTime = Math.min(serverQueue.currentSong().song.duration, seekTime);
+    const originalSeekTime = seekTime - serverQueue.audioResource!.playbackDuration / 1000;
+    await serverQueue.play(seekTime);
+    log(`Fast-forwarding ${seekTime}`);
+    return message.channel.send({ embeds: [defaultEmbed().setDescription(`Fast-Forwarding \`${formatDuration(originalSeekTime)}\``)] });
+};
+
+export const names = ['fastforward', 'ff'];
+export let help = new CommandHelpProvider('fastforward')
+    .setDescription('Fast forward a specified ammount of time in the current song')
+    .setSyntax([new CommandArgument('time', CommandArgumentNecessity.Necessary)]);
+export const requirements = COMMAMD_REQUIREMENTS.REQUIRE_QUEUE_NON_EMPTY | COMMAMD_REQUIREMENTS.REQUIRE_USER_IN_VC;
