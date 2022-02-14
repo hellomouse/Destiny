3f10a1bcd6a9fffe18f4325017457ba61ce9a402
wolfy1339 <webmaster@wolfy1339.com>
2022-02-13 19:34:36 -0500

    Re-do VoiceStateUpdate handling

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -15,22 +15,30 @@ export default async (oldVoiceState: VoiceState, newVoiceState: VoiceState) => {
     let serverQueue = queueManager.get(newVoiceState.guild.id);
     if (serverQueue === undefined) return;
 
-    // Check if the new state has a channel, and that the channel matches the linked channel
-    // Update it if it doesn't match
-    if (newVoiceState.channel && newVoiceState.channel.id !== serverQueue.voiceChannel.id)
-        serverQueue.voiceChannel = newVoiceState.channel;
-
     let voiceChannel = serverQueue.voiceChannel;
     const clientId = newVoiceState.guild.me!.id;
-    const isInVC = voiceChannel.members.has(clientId);
 
-    if (isInVC)
-        if (voiceChannel.members.size === 1)
-            serverQueue.inactivityHelper.onAlone();
-        else
-            serverQueue.inactivityHelper.onPersonJoin();
-    else if (newVoiceState.member?.id === clientId) {
-        queueManager.remove(serverQueue.serverID);
-        log(`Left the channel ${voiceChannel.name}`);
-    }
+    if (oldVoiceState.channel === null)
+        log('Someone joined the Voice Channel');
+    else if (newVoiceState.channel === null)
+        if (newVoiceState.member!.id === clientId) {
+            queueManager.remove(serverQueue.serverID);
+            log(`Left the channel ${voiceChannel.name}`);
+        } else
+            log('Someone left the Voice Channel');
+    else if (oldVoiceState.channel !== null && newVoiceState.channel !== null)
+        if (newVoiceState.member!.id === clientId) {
+            // Check if the new state has a channel, and that the channel matches the linked channel
+            // Update it if it doesn't match
+            if (oldVoiceState.channel.id !== newVoiceState.channel.id) {
+                log(`Moved Voice channel from ${oldVoiceState.channel.name} to ${newVoiceState.channel.name}`);
+                serverQueue.voiceChannel = newVoiceState.channel;
+            }
+        // eslint-disable-next-line curly
+        } else {
+            if (voiceChannel.members.size === 1)
+                serverQueue.inactivityHelper.onAlone();
+            else
+                serverQueue.inactivityHelper.onPersonJoin();
+        }
 };
