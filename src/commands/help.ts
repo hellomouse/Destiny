972b73a03b4e39c93223a941be6dcc16af827c9b
wolfy1339 <webmaster@wolfy1339.com>
2022-06-24 14:58:01 -0400

    Don't use `SelectMenuOptionBuilder` as it is broken

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ComponentType, SelectMenuBuilder, SelectMenuOptionBuilder } from 'discord.js';
+import { ActionRowBuilder, ComponentType, SelectMenuBuilder } from 'discord.js';
 import { CommandHelpProvider } from '../commands.js';
 import { defaultEmbed, warningEmbed } from '../embeds.js';
 import type { Client } from '../types';
@@ -28,13 +28,14 @@ export const run = async (client: Client, message: Message, args: Array<string>)
             .setPlaceholder('Select a command to view it\'s usage');
 
         for (let [commandName, help] of detailedCommandHelp)
-            selectMenu.addOptions(new SelectMenuOptionBuilder(
+            // For some reason, SelectMenuOptionBuilder does not work here, so we instead pass in the data directly
+            selectMenu.addOptions(
                 {
                     label: commandName,
                     value: commandName,
                     description: help.getDescription()
                 }
-            ));
+            );
 
         const row = new ActionRowBuilder<SelectMenuBuilder>().addComponents(selectMenu);
         let sentMessage = await message.channel.send({
